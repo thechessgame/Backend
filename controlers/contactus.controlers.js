443cdad79ede24c251a456af9877e34cdc4d653d
vivek <vivek.reck@gmail.com>
2021-08-28 12:42:52 +0530

    improve contactus api

--- controlers/contactus.controlers.js
+++ controlers/contactus.controlers.js
@@ -5,13 +5,11 @@ import { sendMail } from "../services/mailer.js";
 import { emailHtml } from "../configs/constants.js";
 
 export const postMail = async (req, res, next) => {
-    const email = req.body.email;
-    const name = req.body.name;
-    const comment = req.body.comment;
+    const { email, name, comment } = req.body;
     try {
         const contactUs = new ContactUs({ email, name, comment })
         await contactUs.save()
-        sendMail(email, 'Contact Us', emailHtml.ContactUs(name))
+        await sendMail(email, 'Contact Us', emailHtml.contactUs(name))
         res.created('Request save', null, 'Your Request successfully Send!')
     } catch (err) {
         return res.error(err, null, `Something went wrong!<br>Plz try again later...`)
